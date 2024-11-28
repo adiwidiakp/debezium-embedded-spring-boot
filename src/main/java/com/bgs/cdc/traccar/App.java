7fba62256d9afdb4ee81d87bc3b925846500f4d7
Adhari C Mahendra <adhari.mahendra@gmail.com>
2024-11-29 01:49:37 +0700

    avoid null pointer when access payload under DebeziumRecordUtils.getRecordStructValue() method

--- src/main/java/com/bgs/cdc/traccar/App.java
+++ src/main/java/com/bgs/cdc/traccar/App.java
@@ -2,11 +2,15 @@ package com.bgs.cdc.traccar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class App {
 
+  //public static ConfigurableApplicationContext ctx;  
+
   public static void main(String[] args) {
+    //App.ctx = SpringApplication.run(App.class, args);
     SpringApplication.run(App.class, args);
   }
 
